5fc047261f218a2c0fb4a06b4a5e952cbdfd0d31
Raja <raja@raja.com>
2023-04-17 21:36:51 +0530

    all done

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,9 @@ use App\Http\Controllers\Admin\HolidayController;
 use App\Http\Controllers\Admin\EmployeeController;
 use App\Http\Controllers\Admin\MembershipController;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\ProfileController;
 
-Route::get('/', function () {
-    return view('auth.login');
-});
+
 
 Route::prefix('admin')->as('admin.')->middleware('auth')->group(function () {
 
@@ -60,3 +59,15 @@ Route::prefix('admin')->as('admin.')->middleware('auth')->group(function () {
 Route::controller(LoginController::class)->as('login.')->prefix('login/')->group(function () {
     Route::match(['get', 'post'], '/', 'authentication')->name('authentication');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
