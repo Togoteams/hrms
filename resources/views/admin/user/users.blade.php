5babbe9e5de149a3f0e9bfc984a67f9ccfd067d9
Amit Prajapati <78385573+amitconnect18@users.noreply.github.com>
2023-04-18 09:33:33 +0530

    Update users.blade.php

--- resources/views/admin/user/users.blade.php
+++ resources/views/admin/user/users.blade.php
@@ -125,49 +125,49 @@
                             "searchInDropdown": false,
                             "hideSearch": true
                           }'>
-                                        <option value="10">10</option>
-                                        <option value="15" selected>15</option>
-                                        <option value="20">20</option>
-                                    </select>
-                                </div>
-                                <!-- End Select -->
+                  <option value="10">10</option>
+                  <option value="15" selected>15</option>
+                  <option value="20">20</option>
+                </select>
+              </div>
+              <!-- End Select -->
 
-                                <span class="text-secondary me-2">of</span>
+              <span class="text-secondary me-2">of</span>
 
-                                <!-- Pagination Quantity -->
-                                <span id="datatableWithPaginationInfoTotalQty"></span>
-                            </div>
-                        </div>
-                        <!-- End Col -->
-
-                        <div class="col-sm-auto">
-                            <div class="d-flex justify-content-center justify-content-sm-end">
-                                <!-- Pagination -->
-                                <nav id="datatablePagination" aria-label="Activity pagination"></nav>
-                            </div>
-                        </div>
-                        <!-- End Col -->
-                    </div>
-                    <!-- End Row -->
-                </div>
-                <!-- End Footer -->
+              <!-- Pagination Quantity -->
+              <span id="datatableWithPaginationInfoTotalQty"></span>
             </div>
-            <!-- End Card -->
+          </div>
+          <!-- End Col -->
+
+          <div class="col-sm-auto">
+            <div class="d-flex justify-content-center justify-content-sm-end">
+              <!-- Pagination -->
+              <nav id="datatablePagination" aria-label="Activity pagination"></nav>
+            </div>
+          </div>
+          <!-- End Col -->
         </div>
-        <!-- End Content -->
+        <!-- End Row -->
+      </div>
+      <!-- End Footer -->
+    </div>
+    <!-- End Card -->
+  </div>
+  <!-- End Content -->
 
 
-    </main>
+</main>
 
 
 
-    <div class="modal fade" id="editUserModal" tabindex="-1" aria-labelledby="editUserModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered modal-lg">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="editUserModalLabel">Edit user</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
+<div class="modal fade" id="editUserModal" tabindex="-1" aria-labelledby="editUserModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="editUserModalLabel">Edit user</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
 
       <!-- Body -->
       <div class="modal-body">
@@ -178,99 +178,86 @@
           <div class="row mb-4">
             <label for="editFirstNameModalLabel" class="col-sm-3 col-form-label form-label">Full name <i class="tio-help-outlined text-body ms-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Displayed on public forums, such as Front."></i></label>
 
-                            <div class="col-sm-9">
-                                <div class="input-group input-group-sm-vertical">
-                                    <input type="text" class="form-control" name="first_name"
-                                        id="first_name" placeholder="Your first name"
-                                        aria-label="Your first name" >
-                                    <input type="text" class="form-control" name="last_name"
-                                        id="last_name" placeholder="Your last name"
-                                        aria-label="Your last name">
-                                </div>
-                            </div>
-                        </div>
-                        <!-- End Form -->
+            <div class="col-sm-9">
+              <div class="input-group input-group-sm-vertical">
+                <input type="text" class="form-control" name="editFirstNameModal" id="editFirstNameModalLabel" placeholder="Your first name" aria-label="Your first name" value="Ella">
+                <input type="text" class="form-control" name="editLastNameModal" id="editLastNameModalLabel" placeholder="Your last name" aria-label="Your last name" value="Lauda">
+              </div>
+            </div>
+          </div>
+          <!-- End Form -->
 
-                        <!-- Form -->
-                        <div class="row mb-4">
-                            <label for="editEmailModalLabel" class="col-sm-3 col-form-label form-label">Email</label>
+          <!-- Form -->
+          <div class="row mb-4">
+            <label for="editEmailModalLabel" class="col-sm-3 col-form-label form-label">Email</label>
 
-                            <div class="col-sm-9">
-                                <input type="email" class="form-control" name="email"
-                                    id="email" placeholder="Email" aria-label="Email"
-                                   >
-                            </div>
-                        </div>
-                        <!-- End Form -->
+            <div class="col-sm-9">
+              <input type="email" class="form-control" name="editEmailModal" id="editEmailModalLabel" placeholder="Email" aria-label="Email" value="ella@site.com">
+            </div>
+          </div>
+          <!-- End Form -->
 
-                        <!-- Form -->
-                        <div class="row mb-4">
-                            <label for="mobile" class="col-sm-3 col-form-label form-label">Phone</label>
+          <!-- Form -->
+          <div class="row mb-4">
+            <label for="editPhoneLabel" class="col-sm-3 col-form-label form-label">Phone</label>
 
-                            <div class="col-sm-12">
-                                <div class="input-group input-group-sm-vertical">
-                                    <input type="text" class="js-masked-input form-control" name="mobile"
-                                        id="mobile" placeholder="+x(xxx)xxx-xx-xx" aria-label="+x(xxx)xxx-xx-xx"
-                                       
-                                        data-hs-mask-options='{
+            <div class="col-sm-9">
+              <div class="input-group input-group-sm-vertical">
+                <input type="text" class="js-masked-input form-control" name="phone" id="editPhoneLabel" placeholder="+x(xxx)xxx-xx-xx" aria-label="+x(xxx)xxx-xx-xx" value="1(609)972-22-22" data-hs-mask-options='{
                                  "template": "+0(000)000-00-00"
                                }'>
 
-                                    <!-- Select -->
-                                   
-                                    <!-- End Select -->
-                                </div>
-                            </div>
-                        </div>
-                        <!-- End Form -->
-
-                       
-
-                        <!-- Form -->
-                        <div class="row mb-4">
-                            <label for="username"
-                                class="col-sm-3 col-form-label form-label">User name</label>
-                            <div class="col-sm-9">
-                                <input type="text" class="form-control" name="username"
-                                    id="username" placeholder="Your department"
-                                    aria-label="User Name">
-                            </div>
-                        </div>
-                        <!-- End Form -->
-                        <!-- Form -->
-                        <div class="row mb-4">
-                            <label for="password"
-                                class="col-sm-3 col-form-label form-label">Password</label>
-                            <div class="col-sm-9">
-                                <input type="text" class="form-control" name="password"
-                                    id="password" placeholder="Password"
-                                    aria-label="Password">
-                            </div>
-                        </div>
-                        <div class="row mb-4">
-                            <label for="confirm_password"
-                                class="col-sm-3 col-form-label form-label">Confirm Password</label>
-                            <div class="col-sm-9">
-                                <input type="text" class="form-control" name="confirm_password"
-                                    id="confirm_password" placeholder="Confirm Password"
-                                    aria-label="Confirm Password">
-                            </div>
-                        </div>
-                        <!-- End Form -->
-
-                        <div class="d-flex justify-content-end">
-                            <div class="d-flex gap-3">
-                                <button type="button" class="btn btn-white" data-bs-dismiss="modal"
-                                    aria-label="Close">Cancel</button>
-                                <button type="submit" class="btn btn-primary">Save changes</button>
-                            </div>
-                        </div>
-                    </form>
-                    <!-- End Nav Scroller -->
+                <!-- Select -->
+                <div class="tom-select-custom">
+                  <select class="js-select form-select" autocomplete="off" name="editPhoneSelect" data-hs-tom-select-options='{
+                                  "searchInDropdown": false,
+                                  "hideSearch": true
+                                }'>
+                    <option value="Mobile" selected>Mobile</option>
+                    <option value="Home">Home</option>
+                    <option value="Work">Work</option>
+                    <option value="Fax">Fax</option>
+                    <option value="Direct">Direct</option>
+                  </select>
                 </div>
-                <!-- End Body -->
+                <!-- End Select -->
+              </div>
             </div>
-        </div>
+          </div>
+          <!-- End Form -->
+
+          <!-- Form -->
+          <div class="row mb-4">
+            <label for="editOrganizationModalLabel" class="col-sm-3 col-form-label form-label">Vendor</label>
+
+            <div class="col-sm-9">
+              <input type="text" class="form-control" name="editOrganizationModal" id="editOrganizationModalLabel" placeholder="Your organization" aria-label="Your organization" value="Htmlstream">
+            </div>
+          </div>
+          <!-- End Form -->
+
+          <!-- Form -->
+          <div class="row mb-4">
+            <label for="editDepartmentModalLabel" class="col-sm-3 col-form-label form-label">Department</label>
+
+            <div class="col-sm-9">
+              <input type="text" class="form-control" name="editDepartmentModal" id="editDepartmentModalLabel" placeholder="Your department" aria-label="Your department">
+            </div>
+          </div>
+          <!-- End Form -->
+
+          <div class="d-flex justify-content-end">
+            <div class="d-flex gap-3">
+              <button type="button" class="btn btn-white" data-bs-dismiss="modal" aria-label="Close">Cancel</button>
+              <button type="submit" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </form>
+        <!-- End Nav Scroller -->
+      </div>
+      <!-- End Body -->
     </div>
-    <!-- End Edit user -->
+  </div>
+</div>
+<!-- End Edit user -->
 @endsection
