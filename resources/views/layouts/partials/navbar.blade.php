f338fe47cf50e62ce9868123b8871e9a7077d6e3
Manoj <manojrana19990@gmail.com>
2023-05-02 19:12:16 +0530

    update

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -30,7 +30,7 @@
              <!-- End Navbar Vertical Toggle -->
 
              <!-- Search Form -->
-             <h1 class="page-header-title">HRMS</h1>
+             <h1 class="tittle-nav">HRMS</h1>
 
 
              <!-- End Search Form -->
