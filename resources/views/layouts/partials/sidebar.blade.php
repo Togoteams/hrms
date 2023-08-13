f00811c23d447cafa41c5821eb0d8bb707399ef5
Togo Teams <108182407+Togoteams@users.noreply.github.com>
2023-08-13 20:11:50 +0530

    Update sidebar.blade.php

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -353,7 +353,7 @@
                          </a>
 
                          <div id="emppayroll"
-                             class="nav-collapse collapse {{ show(['payroll.salary.index', 'payroll.payscale.index', 'payroll.head.index','payroll.tax-slab-setting.index',,'payroll.salary-increment-setting.index','payroll.salary-increment-reporting.index','payroll.reimbursement_type.index','payroll.reimbursement.index']) }} "
+                             class="nav-collapse collapse {{ show(['payroll.salary.index', 'payroll.payscale.index', 'payroll.head.index','payroll.tax-slab-setting.index','payroll.salary-increment-setting.index','payroll.salary-increment-reporting.index','payroll.reimbursement_type.index','payroll.reimbursement.index']) }} "
                              data-bs-parent="#navbarVerticalMenuPagesMenu">
 
                              <a class="nav-link {{ Route::getCurrentRoute()->getName() == 'admin.payroll.tax-slab-setting.index' ? 'active' : '' }}  "
