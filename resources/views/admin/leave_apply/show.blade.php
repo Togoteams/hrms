a8e7a132827a9bc539106254362f7a8d2ea2d1dd
Bindu612 <bindushristy@gmail.com>
2023-11-10 09:30:15 +0530

    view blade

--- resources/views/admin/leave_apply/show.blade.php
+++ resources/views/admin/leave_apply/show.blade.php
@@ -1,8 +1,31 @@
 <div class="row">
-    <div class="mb-2 col-sm-6">
+
+
+    <div class="mb-2 col-sm-4">
+        <div class="form-group">
+            <label for="emp_name">Employee name :- </label>
+            <label for="emp_name"> {{ $data->user->name }}</label>
+        </div>
+    </div>
+
+    <div class="mb-2 col-sm-4">
+        <div class="form-group">
+            <label for="emp_name">EC Number :- </label>
+            <label for="emp_name"> {{ $data->user->employee->ec_number }}</label>
+        </div>
+    </div>
+
+    <div class="mb-2 col-sm-4">
         <div class="form-group">
             <label for="leave_type_id">Leave Types :- </label>
             <label for="leave_type_id"> {{ $data->leave_type->name }}</label>
+        </div>
+    </div>
+
+    <div class="mb-2 col-sm-4">
+        <div class="form-group">
+            <label for="leave_type_id">Apply for :- </label>
+            <label for="leave_type_id"> {{ $data->leave_applies_for }}</label>
         </div>
     </div>
 
@@ -16,6 +39,33 @@
         <div class="form-group">
             <label for="end_date">end_date</label>
             <label for="end_date">{{ date('d-m-Y', strtotime($data->end_date)) }}</label>
+        </div>
+    </div>
+
+    <div class="col-sm-4">
+        <div class="form-group">
+            <label for="approved_at">Approved date :- </label>
+            @if(!empty($data->approved_at) && $data->approved_at != '1970-01-01')
+                <label for="approved_at">{{ date('d-m-Y', strtotime($data->approved_at)) }}</label>
+            @else
+                <label for="approved_at">N/A</label>
+            @endif
+        </div>
+    </div>
+    <div class="col-sm-4">
+        <div class="form-group">
+            <label for="rejected_at">Rejected date :- </label>
+            @if(!empty($data->rejected_at) && $data->rejected_at != '1970-01-01')
+                <label for="rejected_at">{{ date('d-m-Y', strtotime($data->rejected_at)) }}</label>
+            @else
+                <label for="rejected_at">N/A</label>
+            @endif
+        </div>
+    </div>
+    <div class="col-sm-4">
+        <div class="form-group">
+            <label for="status">Status :- </label>
+            <label for="status">{{ucfirst($data->status)}}</label>
         </div>
     </div>
 
