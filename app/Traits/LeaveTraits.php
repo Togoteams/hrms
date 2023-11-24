6c79881c23b38ec395cdaa8a550d208d852f1ec2
Amit Prajapati <78385573+amitconnect18@users.noreply.github.com>
2023-11-24 23:54:34 +0530

    Update LeaveTraits.php

--- app/Traits/LeaveTraits.php
+++ app/Traits/LeaveTraits.php
@@ -62,12 +62,12 @@ trait LeaveTraits
           }
         }else
         {
-          if($years>1)
+          if($years>=1)
           {
             $totalWorkingMonths = (date('m') -1);
           }else
           {
-            $totalWorkingMonths = (date('m') -1);
+            $totalWorkingMonths = ($months -1);
           }
           $total_leave = ceil(($perYearLeave / 12) * $totalWorkingMonths);
         }
