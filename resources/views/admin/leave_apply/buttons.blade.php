7b8dda62c08eca618b16d5d7a1377581af03dbd3
amitprajapati <togoteams@gmail.com>
2024-01-15 17:55:43 +0530

    update with few changes

--- resources/views/admin/leave_apply/buttons.blade.php
+++ resources/views/admin/leave_apply/buttons.blade.php
@@ -27,13 +27,14 @@
     @endif
     @if (!isemplooye())
         {{-- @if (approvalBtnEnable($item->id) == 1) --}}
-            @if($item->status=='approved' && $item->is_reversal==0 && $item->user?->employee?->employment_type=="local")
+            @if($item->status=='approved' && $item->is_reversal==0 && $item->user?->employee?->employment_type=="local" )
                 @if($item->leave_type->slug=="leave-without-pay")
                     <button type="button" onclick="reverseLeaveWithoutPay({{$item->id}})"
                     href="#" class="btn btn-edit btn-sm"><i class="fa fa-undo" aria-hidden="true"></i>
                 </button>
                 @endif
             @endif
+            @if($item->approval_authority==auth()->user()->id)
             @can('change-status-leave-apply')
                 <button type="button"
                     @if ($item->status != 'approved') onclick="editForm('{{ route('admin.' . $route . '.status_modal', $item->id) }}', 'statuschange')"
@@ -41,6 +42,7 @@
                     class="btn @if ($item->status == 'pending') btn-warning @elseif ($item->status == 'reject') btn-danger @elseif($item->status == 'approved') btn-success @else btn-secondary @endif btn-sm">
                     {{ ucfirst($item->status) }} </button>
             @endcan
+            @endif 
         {{-- @endif --}}
     @endif
 
