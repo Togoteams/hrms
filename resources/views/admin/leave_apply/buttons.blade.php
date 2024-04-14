efe266b981c4248eb6920bd9e05ced1d6f35d5c7
amitprajapati <togoteams@gmail.com>
2024-04-14 15:01:41 +0530

    Update buttons.blade.php

--- resources/views/admin/leave_apply/buttons.blade.php
+++ resources/views/admin/leave_apply/buttons.blade.php
@@ -34,7 +34,7 @@
                 </button>
                 @endif
             @endif
-            @if($item->approval_authority==auth()->user()->id)
+            {{-- @if($item->approval_authority==auth()->user()->id) --}}
             @can('change-status-leave-apply')
                 <button type="button"
                     @if ($item->status != 'approved') onclick="editForm('{{ route('admin.' . $route . '.status_modal', $item->id) }}', 'statuschange')"
@@ -42,7 +42,7 @@
                     class="btn @if ($item->status == 'pending') btn-warning @elseif ($item->status == 'reject') btn-danger @elseif($item->status == 'approved') btn-success @else btn-secondary @endif btn-sm">
                     {{ ucfirst($item->status) }} </button>
             @endcan
-            @endif 
+            {{-- @endif  --}}
         {{-- @endif --}}
     @endif
 
